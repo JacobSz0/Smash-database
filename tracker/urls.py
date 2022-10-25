c5f83bdb5c3a42e375ee0b0fd32609113040ab15
Jacob <your@email.here>
2022-10-25 09:44:58 -0700

    Feature 7 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("", redirect_main, name="home"),
+    path("accounts/", include("accounts.urls")),
 ]
