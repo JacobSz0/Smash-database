7b962a801951e8ebbdd3dd18063a627dacdb669d
Jacob <your@email.here>
2022-10-25 13:16:11 -0700

    Feature 10 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 def user_login(request):
@@ -29,3 +30,29 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("/accounts/login/")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            password_confirmation = form.cleaned_data['password_confirmation']
+
+            if password == password_confirmation:
+                user = User.objects.create_user(
+                    username,
+                    password=password,
+                )
+                login(request, user)
+                return redirect("list_projects")
+    else:
+        form = SignUpForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "registration/signup.html", context)
+
+    #jac
+    #Samusrules2!
