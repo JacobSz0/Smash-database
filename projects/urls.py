68ba40800568ad0fc96bd655d9496882aa3e3c16
Jacob <your@email.here>
2022-10-25 21:15:50 -0700

    Feature 13 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from projects.views import list_projects
+from projects.views import list_projects, show_project
 
 
 urlpatterns = [
     path("", list_projects, name="list_projects"),
+    path("<int:id>/", show_project, name="show_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
 
@@ -10,4 +10,12 @@ def list_projects(request):
         "list_projects": projects,
     }
     return render(request, "projects/list.html", context)
-# Create your views here.
+
+
+@login_required()
+def show_project(request, id):
+    project = get_object_or_404(Project, id=id)
+    context = {
+        "pet_project": project,
+    }
+    return render(request, "projects/detail.html", context)
